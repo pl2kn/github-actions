cc4524924cb4e91660e4f65f8fbe4f9fc1fee5fc
Pavel Konkin <pavel.konkin.dev@gmail.com>
2025-06-14 12:05:33 -0400

    init 2

--- app/src/test/kotlin/org/example/AppTest.kt
+++ app/src/test/kotlin/org/example/AppTest.kt
@@ -10,6 +10,7 @@ class AppTest {
     @Test
     fun appHasAGreeting() {
         val classUnderTest = App()
+        
         assertNotNull(classUnderTest.greeting, "app should have a greeting")
     }
 }
